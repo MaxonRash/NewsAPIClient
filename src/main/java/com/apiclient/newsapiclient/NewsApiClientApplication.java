7cf9fb0dd4561659fca8ca2043fe64fde78e9020
MaxonRash <rashnikov.m@yandex.com>
2024-10-24 18:36:42 +0300

    Initial commit

---
+++ src/main/java/com/apiclient/newsapiclient/NewsApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.apiclient.newsapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsApiClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apiclient/newsapiclient/NewsApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiclient.newsapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
